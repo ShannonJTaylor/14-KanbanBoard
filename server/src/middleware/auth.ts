1f84f7231b2848e5c9a4bfa56839f75acbcc2220
ShannonJTaylor <shannonjo8475@gmail.com>
2025-03-11 04:19:31 -0400

    update routes

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -2,9 +2,35 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 interface JwtPayload {
-  username: string;
+  user: any;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  // TODO: verify the token exists and add the user data to the request object
+export const authenticateToken = async (req: Request, res: Response, next: NextFunction) => {
+    // TODO: verify the token exists and add the user data to the request object
+    const authHeader = req.headers.authorization
+    console.log(req.headers)
+    if (!authHeader) {
+      res.status(401).json({
+        success: false, error: "NOT AUTHENTICATED"
+      })
+      return
+    }
+    try {
+      const verifiedJWT = await jwt.verify(authHeader as string, process.env.JWT_SECRET_KEY!) as JwtPayload
+    if (!verifiedJWT) {
+      res.status(401).json({
+        success: false, error: "NOT AUTHENTICATED"
+      })
+      return
+    }
+    console.log(verifiedJWT)
+    req.user=verifiedJWT?.user
+    next()
+    } catch (error) {
+      console.log(error)
+      res.status(401).json({
+        success: false, error: "NOT AUTHENTICATED"
+      })
+      return
+    } 
 };
